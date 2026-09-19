7834fb1d5d1abda126c73d47c2fde64445a729de
Marcos Escalera <marcosescalera1213@gmail.com>
2026-09-19 17:00:02 -0300

    1er commit enviroments

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  supabase: {
+    // URL base de tu proyecto Supabase (sin /rest/v1/)
+    // cambiar por la URL de nuestra base de datos (Hecho)
+    url: 'https://unursbovuxtkndczboou.supabase.co',
+    // CAMbiar por nuestra clave (hecho)
+    publicKey: 'sb_publishable_n-kAAahUdKe4f7AiekZQOQ_GTAqMsF3'
+  }
+};
